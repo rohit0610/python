d5f6d0dafa3c2ba2ff3fd56b2ed40cf9652f5944
Rohit Vishwakarma <41201199+rohit0610@users.noreply.github.com>
2020-08-30 14:47:49 +0530

    Add files via upload

---
+++ boolean.py
@@ -0,0 +1,8 @@
+pineapples =5
+zebras =2
+zebras<pineapples
+pineapples<zebras
+pineapples==zebras
+(pineapples==3) and (zebras==2)
+(pineapples == 3) or (zebras == 2)
+pineapples != zebras
---
+++ if.py
@@ -0,0 +1,5 @@
+age=10
+if age==10:
+   print('You are 10 years old.')
+else:
+   print('You are not 10 years old.')
